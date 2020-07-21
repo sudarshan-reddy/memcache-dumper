2f04f563816b72139f0ac05780908b843711df7d
Sudarsan <sudar.theone@gmail.com>
2020-07-21 21:45:11 +0800

    saving scratch main

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use memcache::Client;
-use std::io::Write;
+use std::io::{Read, Write};
 pub struct Dumper {
     client: Client,
 }
@@ -13,6 +13,15 @@ impl Dumper {
     pub fn dump_key_to_file<W: Write>(&mut self, key: &str, mut writer: W) -> Result<(), Error> {
         let value: Option<Vec<u8>> = self.client.get(key)?;
         value.and_then(|v| Some(writer.write(&v).ok()?));
+        Ok(())
+    }
+
+    pub fn load_key_from_file<R: Read>(&mut self, key: &str, mut reader: R) -> Result<(), Error> {
+        let mut data: Vec<u8> = Vec::new();
+        reader.read_to_end(&mut data)?;
+
+        let c: &[u8] = &data;
+        self.client.set(key, c, 0)?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
 pub mod lib;
-fn main() {}
+use lib::Dumper;
+use std::fs::File;
+fn main() {
+    let mut d = Dumper::new("memcache://127.0.0.1:11211?timeout=10&tcp_nodelay=true")
+        .expect("dumper init failed");
+    let f = File::open("test.file").expect("open failed");
+    d.load_key_from_file("test", f).expect("load failed");
+}
