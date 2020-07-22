cdd891b226a40790e5dd387dd3a9c51ce18b40f9
Sudarsan <sudar.theone@gmail.com>
2020-07-22 15:41:16 +0800

    file read done

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod lib;
 use clap::{App, Arg};
 use lib::Dumper;
 use std::fs::File;
+use std::io;
 fn main() {
     let matches = App::new("memcache-dumper")
         .version("0.0.1")
@@ -18,15 +19,44 @@ fn main() {
             Arg::with_name("value")
                 .about("value or file name")
                 .index(2)
-                .required(true),
+                .required(false),
         ])
         .get_matches();
-    println!("{}", matches.value_of("key").unwrap());
+    let key = matches
+        .value_of("key")
+        .expect("first argument <key> invalid");
+
+    let value = matches
+        .value_of("value")
+        .expect("second argument <value> invalid");
+
+    let is_dump = matches.is_present("d");
+    let is_load = matches.is_present("l");
+    if is_dump && is_load {
+        panic!("both dump and load cant be set");
+    }
+
+    let is_value_from_file = matches.is_present("f");
+
     let mut d = Dumper::new("memcache://127.0.0.1:11211?timeout=10&tcp_nodelay=true")
         .expect("dumper init failed");
-    let f = File::open("test.file").expect("open failed");
-    d.load_key_from_file("test", f).expect("load failed");
 
-    let f2 = File::create("op.file").expect("create f2 failed");
-    d.dump_key_to_file("test", f2).expect("save failed");
+    if is_dump {
+        if is_value_from_file {
+            let f = File::create(value).expect("create failed");
+            d.dump_key_to_file(key, f).expect("dump to file failed");
+            return;
+        }
+        d.dump_key_to_file(key, io::stdout())
+            .expect("dump to stdout failed");
+        return;
+    }
+
+    if is_load {
+        if !is_value_from_file {
+            panic!("not implemented yet")
+        }
+        let f = File::open("test.file").expect("open failed");
+        d.load_key_from_file("test", f).expect("load failed");
+    }
 }
